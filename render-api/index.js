5d9f1b90e742f93e1395f3b521e1e4d55d2d1901
Sirajalishaik <143028787+Sirajalishaik@users.noreply.github.com>
2026-02-04 16:59:44 +0530

    Update index.js

--- render-api/index.js
+++ render-api/index.js
@@ -3,9 +3,8 @@ const axios = require("axios");
 
 const app = express();
 
-// ⚠️ CHANGE SHEET NAME IF NEEDED
 const SHEET_ID = "2PACX-1vSz6yxxLfrAYP-n7R8tqgV9WQp6K4unu8IUtY-_Vkj_EGhLyNj5C5YxAkReDEgjfg-SXNbipajncJqm";
-const SHEET_NAME = "Sheet1";
+const SHEET_NAME = "Sheet1"; // change only if tab name differs
 
 app.get("/data", async (req, res) => {
   const url = `https://opensheet.elk.sh/${SHEET_ID}/${SHEET_NAME}`;
